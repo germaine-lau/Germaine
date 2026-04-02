5165dec9f7d33f100ed19c63d6d9655321db00a1
Germaine Lau <hello@germainelau.com>
2026-04-02 13:25:50 -0700

    Fix video gradient overlay sizing (remove bottom grey strip on mobile)

--- src/components/ProjectRowOverlay.jsx
+++ src/components/ProjectRowOverlay.jsx
@@ -835,7 +835,7 @@ function ProjectRowOverlay({
 
                 {!shouldUsePreviewBehavior && (
                   <>
-                    <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-black/20 via-transparent to-transparent opacity-0 transition-opacity duration-200 group-hover:opacity-100" />
+                   <div className="pointer-events-none absolute inset-x-0 bottom-0 h-[120px] bg-gradient-to-t from-black/30 to-transparent opacity-0 transition-opacity duration-200 group-hover:opacity-100" />
 
                     <div className="pointer-events-none absolute inset-0 z-10 opacity-80 transition-opacity duration-200 group-hover:opacity-100">
                     <div className="absolute bottom-2 left-0 right-0 px-3">
